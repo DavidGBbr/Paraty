34ea289de4687d60d8580b27e1e7a0a8a3631955
DavidGBbr <davidgomesbrigido@gmail.com>
2022-07-28 20:10:50 -0300

    tela passeios styling

--- componentes/TelaPasseios/index.js
+++ componentes/TelaPasseios/index.js
@@ -1,32 +1,34 @@
 import React from 'react';
 import { View, Text, Image } from 'react-native';
+import styles from './styles';
 import imgPasseio1 from '../../assets/passeio01.png';
 import imgPasseio2 from '../../assets/passeio02.png';
 import imgPasseio3 from '../../assets/passeio03.png';
 
 export default function TelaPasseios() {
  return (
-   <View>
+   <View style={styles.container}>
 
-    <Text>Passeios em Paraty</Text>
-    <Text>O município de Paraty está quase inteiro em área de parques e de preservação ambiental. Muitas são as 
+    <Text style={styles.title}>Passeios em Paraty</Text>
+    <Text style={styles.description}>O município de Paraty está quase inteiro em área de parques e de preservação ambiental. Muitas são as 
     opções de passeios para quem curte a natureza: passeios de jeep, de bike, caminhadas por trilhas entre outros. 
     Veja dois exemplos:</Text>
 
-    <View>
-      <Text>Tirolesa</Text>
-      <Image source={imgPasseio1}/>
+    <View style={styles.card}>
+      <Text cardTitle>Tirolesa</Text>
+      <Image source={imgPasseio1} style={styles.img}/>
     </View>
 
-    <View>
-      <Text>Rafting</Text>
-      <Image source={imgPasseio2}/>
+    <View style={styles.card}>
+      <Text cardTitle>Rafting</Text>
+      <Image source={imgPasseio2} style={styles.img}/>
     </View>
 
-    <View>
-      <Text>Conoagem no mangue</Text>
-      <Image source={imgPasseio3}/>
+    <View style={styles.card}>
+      <Text cardTitle>Conoagem no mangue</Text>
+      <Image source={imgPasseio3} style={styles.img}/>
     </View>
+
    </View>
   );
 }
---
+++ componentes/TelaPasseios/styles.js
@@ -0,0 +1,39 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container:{
+        padding:20,
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor:'#f4fbc3',
+    },
+
+    title:{
+        fontSize:20,
+        fontWeight:'bold',
+        marginBottom:5,
+    },
+
+    description:{
+        textAlign:'center',
+        marginBottom:20,
+    },
+
+    card:{
+        backgroundColor:'#fff',
+        width:320,
+        marginBottom:5,
+        padding:10,
+        borderRadius:5,
+        borderColor:'#979797',
+        borderWidth:1,
+    },
+
+    img:{
+        width:300,
+        height:80,
+    },
+})
+
+export default styles;
