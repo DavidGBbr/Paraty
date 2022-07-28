888bac831b09854c121ed6737503e1b3b6d776e0
DavidGBbr <davidgomesbrigido@gmail.com>
2022-07-28 12:39:01 -0300

    create tela inicial

--- componentes/TelaInicial/index.js
+++ componentes/TelaInicial/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, Text, Image, Button } from 'react-native';
 
-export default function TelaInicial() {
+import Capa from '../../assets/01.png'
+
+export default function TelaInicial(props) {
  return (
-   <Text>Início</Text>
+   <View>
+
+    <View>
+      <Text>Paraty</Text>
+      <Text>Saiba o que visitar em paraty</Text>
+      <Image source={Capa}/>
+
+      <View>
+        <Button title="Ver Restaurantes" onPress={()=>{props.navigation.navigate('Tela Restaurantes')}}/>
+      </View>
+
+      <View>
+        <Button title="Ver Passeios" onPress={()=>{props.navigation.navigate('Tela Passeios')}}/>
+      </View>
+      
+      <View>
+        <Button title="Ver Hospedagem" onPress={()=>{props.navigation.navigate('Tela Hospedagem')}}/>
+      </View>
+
+    </View>
+
+   </View>
   );
 }
