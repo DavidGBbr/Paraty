4583b919c2f9ccd8ba04dd66db9a6251c415e0dc
DavidGBbr <davidgomesbrigido@gmail.com>
2022-07-28 18:36:27 -0300

    create tela restaurantes

--- componentes/TelaRestaurantes/index.js
+++ componentes/TelaRestaurantes/index.js
@@ -1,8 +1,36 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
+import ImgRestaurante1 from '../../assets/restaurante01.png';
+import ImgRestaurante2 from '../../assets/restaurante02.png';
+import ImgRestaurante3 from '../../assets/restaurante03.png';
 
 export default function TelaRestaurantes() {
  return (
-   <Text>Restaurantes</Text>
+  <View>
+    <Text>Bares e Restaurantes</Text>
+    <Text>O prazer da boa comida você encontra em Paraty. Com inúmeras opções de restaurantes, a
+    cidade oferece o melhor da culinária internacional, juntamente à saborosa cozinha caiçara. 
+    Confira alguns bares e restaurantes famosos.</Text>
+
+    <View>
+      <Image source={ImgRestaurante1}/>
+      <Text>Armazém Mar</Text>
+      <Text>Localização: Rod Rio-Santos</Text>
+    </View>
+
+    <View>
+      <Image source={ImgRestaurante2}/>
+      <Text>Bendita's Restaurante</Text>
+      <Text>Localização: Centro Histórico</Text>
+    </View>
+
+    <View>
+      <Image source={ImgRestaurante3}/>
+      <Text>Seria do mar pizza-bar</Text>
+      <Text>Localização: Praia do Jabaquara</Text>
+    </View>
+
+
+  </View>
   );
 }
